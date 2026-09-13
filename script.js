d98cd1e70cd331defcb6f46ef93fbac56d54c7d6
sarisahmed92 <saris.ahmed92@gmail.com>
2026-09-12 21:46:40 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll(".youtube-preview").forEach((preview) => {
+    preview.querySelector("button").addEventListener("click", () => {
+        const videoId = preview.dataset.videoId;
+
+        preview.innerHTML = `
+            <iframe
+                src="https://www.youtube-nocookie.com/embed/${I4ZadlDJAZg}?autoplay=1&rel=0"
+                title="YouTube video player"
+                frameborder="0"
+                allow="autoplay; encrypted-media; picture-in-picture"
+                allowfullscreen>
+            </iframe>
+        `;
+    });
+});
