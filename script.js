2b9057239136344474376fd9fce9e232192e705a
Saris <saris@pop-os.lan>
2026-09-13 23:53:29 -0700

    Update website

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 console.log("script loaded");
-document.querySelectorAll(".youtube-preview").forEach((preview) => {
-    preview.querySelector("button").addEventListener("click", () => {
-        console.log("clicked", videoId);
-        const videoId = preview.dataset.videoId;
 
-        preview.innerHTML = `
-            <iframe
-                width="1280"
-                height="720"
-                src="https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1&rel=0"
-                title="YouTube video player"
-                frameborder="0"
-                allow="autoplay; encrypted-media; picture-in-picture"
-                allowfullscreen>
-            </iframe>
-        `;
-    });
+document.querySelectorAll(".youtube-preview").forEach((preview) => {
+  const button = preview.querySelector("button");
+
+  button.addEventListener("click", () => {
+    console.log("clicked");
+
+    const videoId = preview.dataset.videoId;
+
+    preview.innerHTML = `
+      <iframe
+        src="https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1&rel=0"
+        title="YouTube video player"
+        allow="autoplay; encrypted-media; picture-in-picture"
+        allowfullscreen>
+      </iframe>
+    `;
+  });
 });
